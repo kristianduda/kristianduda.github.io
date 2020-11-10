d13ecbd2b2a7303a469031f727b99f9625c3457c
kristianduda <kristianduda83@gmail.com>
2020-11-10 16:17:41 +0100

    init

---
+++ app/precache-manifest.1c3c6a04b5ff7aa118a1fba20325956d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "127917d16c2343d0c8fa",
+    "url": "/app/static/js/runtime~main.d465ad9e.js"
+  },
+  {
+    "revision": "c3636e0a52567cadfa25",
+    "url": "/app/static/js/main.977c740d.chunk.js"
+  },
+  {
+    "revision": "d443e31338aaf6c3824b",
+    "url": "/app/static/js/2.a5fe2894.chunk.js"
+  },
+  {
+    "revision": "c3636e0a52567cadfa25",
+    "url": "/app/static/css/main.c3684d03.chunk.css"
+  },
+  {
+    "revision": "d443e31338aaf6c3824b",
+    "url": "/app/static/css/2.4b7adf07.chunk.css"
+  },
+  {
+    "revision": "bd6c66f3c6fdd4a24ddd0a66b715e3dd",
+    "url": "/app/index.html"
+  }
+];
